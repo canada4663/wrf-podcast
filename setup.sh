adace0c88a75408962a2c6cac6a252f7214dd31b
EC2 Default User <ryanhoium@gmail.com>
2016-11-28 03:11:38 +0000

    Bugs in setup.sh and requirements.txt

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 sudo yum update
-sudo yum install git setuptools python-devel libxml2-devel libxml2 libxslt-devel libxslt
+sudo yum install git python-devel libxml2-devel libxml2 libxslt-devel libxslt
+sudo pip install setuptools virtualenv
 git clone https://github.com/canada4663/wrf-podcast.git
-pip install virtualenv
 cd wrf-podcast
 virtualenv .
 source bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 sh refreshDeployment.sh
